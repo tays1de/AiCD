322ffdf7246f218a54ea46bcf1fdd2d23ba06d68
tays1de <why_n0t_13@mail.ru>
2020-04-28 11:00:59 +0300

    Lab1

---
+++ LabAICD1/LabAICD1/LabAICD1.cpp
@@ -0,0 +1,127 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class day {
+
+private:
+	int daybuy;
+	int daysell;
+	int max;
+
+public:
+
+	day() {
+		daybuy = 0;
+		daysell = 0;
+		max = 0;
+	}
+
+	int getbuy() {
+		return daybuy;
+	}
+
+	int getsell() {
+		return daysell;
+	}
+
+	int getmax() {
+		return max;
+	}
+
+	void setbuy(int daybuy) {
+		this->daybuy = daybuy;
+	}
+
+	void setsell(int daysell) {
+		this->daysell = daysell;
+	}
+
+	void setmax(int max) {
+		this->max = max;
+	}
+
+	day find(int* mas, int l, int r) {
+		day temp;
+		int MaxSum;
+		int MaxSum1 = -99999;
+		int MaxSum2 = -99999;
+		int sum1 = 0;
+		int mid = (l + r) / 2;
+		int daybuy = 0;
+		int daysell = 0;
+		for (int i = mid; i >= l; i--) {
+			sum1 += mas[i];
+			if (sum1 > MaxSum1) {
+				MaxSum1 = sum1;
+				daybuy = i + 1;
+			}
+		}
+		int sum2 = 0;
+		for (int i = mid + 1; i <= r; i++) {
+			sum2 += mas[i];
+			if (sum2 > MaxSum2) {
+				MaxSum2 = sum2;
+				daysell = i + 1;
+			}
+		}
+		MaxSum = MaxSum1 + MaxSum2;
+		temp.setbuy(daybuy);
+		temp.setsell(daysell + 1);
+		temp.setmax(MaxSum);
+		return temp;
+	}
+	day rekursiya(int* mas, int l, int r) {
+		day mid, leftmid, rightmid;
+		if (l == r) {
+			day value;
+			value.setbuy(l + 1);
+			value.setsell(l + 2);
+			value.setmax(mas[l]);
+			return value;
+		}
+		else {
+			int midl = (l + r) / 2;
+			mid = find(mas, l, r);
+			leftmid = rekursiya(mas, l, midl);
+			rightmid = rekursiya(mas, midl + 1, r);
+			if (mid.getmax() >= leftmid.getmax() && mid.getmax() >= rightmid.getmax()) {
+				return mid;
+			}
+			else {
+				if (leftmid.getmax() >= mid.getmax() && leftmid.getmax() >= rightmid.getmax()) {
+					return leftmid;
+				}
+				else {
+					return rightmid;
+				}
+
+			}
+		}
+	}
+
+};
+
+int main() {
+	setlocale(0, "");
+	day temp;
+	int a;
+	cout << "Введите число дней: ";
+	cin >> a;
+	int* mas = new int[a];
+	int* mas1 = new int[a - 1];
+	for (int i = 0; i < a; i++) {
+		cout << "День " << i + 1 << " введите стоимость акций: ";
+		cin >> mas[i];
+	}
+	for (int i = 0; i < a - 1; i++) {
+		mas1[i] = mas[i + 1] - mas[i];
+	}
+	day max = temp.rekursiya(mas1, 0, a - 1);
+	cout << "День покупки: " << max.getbuy()-1 << endl << "День продажи: " << max.getsell() << endl;
+	cout << "Максимальная прибыль с акции: " << max.getmax() << endl;//если отрицательная то прибыли нет(надо поставить условие)
+	delete[]mas;
+	delete[]mas1;
+	system("pause");
+	return 0;
+}
---
+++ LabAICD1/zad2/zad2.cpp
@@ -0,0 +1,178 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Bottle {
+private:
+    int Volume;
+    int nowVolume;
+    char name;
+
+public:
+    Bottle(int Volum, char n) {
+        Volume = Volum;
+        nowVolume = 0;
+        name = n;
+    }
+    void setNowVolume(int Vol) {
+        this->nowVolume = Vol;
+    }
+
+    int getNowVolume() {
+        return nowVolume;
+    }
+
+    int getVolume() {
+        return Volume;
+    }
+
+    void FullVolume() {
+        nowVolume = Volume;
+        cout << "Объём полностью наполнен в " << this->name << endl;
+}
+    void Clear() {
+        this->nowVolume = 0;
+        cout << "Объём очищен в " << this->name  << endl;
+    }
+
+};
+
+
+void perelivanie(Bottle *A, Bottle *B) {
+    if (A->getNowVolume() + B->getNowVolume() <= B->getVolume()) {
+        B->setNowVolume(B->getNowVolume() + A->getNowVolume());
+        A->setNowVolume(0);
+    }
+    if (A->getNowVolume() + B->getNowVolume() > B->getVolume()) {
+        int buff = A->getNowVolume() + B->getNowVolume() - B->getVolume();
+        B->setNowVolume(B->getVolume());
+        A->setNowVolume(buff);
+    }
+    cout << "Объём в сосуде А: " << A->getNowVolume() << " \n Объём в сосуде Б: " << B->getNowVolume() << endl;
+}
+
+
+int nod(int a, int b)
+{
+    int max, min;
+    if (a > b)
+    {
+        max = abs(a);
+        min = abs(b);
+    }
+    else
+    {
+        max = abs(b);
+        min = abs(a);
+    }
+    int r = max % min;
+    if (r == 0)
+    {
+        return min;
+    }
+    else
+    {
+        return nod(b, r);
+    }
+}
+
+
+void nedNowVolume(int X, Bottle A, Bottle B) {
+
+    bool first = true;
+    if (X > A.getVolume() || X > B.getVolume() && (nod(A.getVolume(), B.getVolume()) != 1)  || (nod(A.getVolume(), B.getVolume()) == 2 && X % 2 != 0)) {
+        cout << "Невозможно получить такой объём" << endl;
+    }
+    else if (nod(A.getVolume(), B.getVolume()) == 1) {
+         
+        if (A.getVolume() > B.getVolume()) {
+
+            while (first) {
+                A.FullVolume();
+                perelivanie(&A, &B);
+
+                if (A.getNowVolume() == X || B.getNowVolume() == X) {
+                    cout << "Получилось!" << endl;
+                    first = false;
+                    continue;
+
+                }
+
+                B.Clear();
+                perelivanie(&A, &B);
+                if (A.getNowVolume() == X || B.getNowVolume() == X) {
+                    cout << "Получилось!" << endl;
+                    first = false;
+
+                }
+                if (B.getNowVolume() == B.getVolume() && A.getNowVolume() != 0) {
+                    B.Clear();
+                    perelivanie(&A, &B);
+                }
+            }
+        }
+
+    }
+    else if (nod(A.getVolume(), B.getVolume()) != 1 && (X % nod(A.getVolume(), B.getVolume()) == 0)) {
+
+        if (A.getVolume() > B.getVolume()) {
+
+            while (first) {
+                A.FullVolume();
+                perelivanie(&A, &B);
+
+                if (A.getNowVolume() == X || B.getNowVolume() == X) {
+                    cout << "Получилось!" << endl;
+                    first = false;
+                    continue;
+
+                }
+
+                B.Clear();
+                perelivanie(&A, &B);
+                if (A.getNowVolume() == X || B.getNowVolume() == X) {
+                    cout << "Получилось!" << endl;
+                    first = false;
+                    continue;
+
+                }
+                if (B.getNowVolume() == B.getVolume() && A.getNowVolume() != 0) {
+                    B.Clear();
+                    perelivanie(&A, &B);
+                }
+            }
+        }
+    }
+
+    }
+
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    int Av, AV, Bv, BV;
+    cout << "Введите объём сосуда А: ";
+    cin >> Av;
+  
+    cout << "Введите объём сосуда Б: ";
+    cin >> Bv;
+    
+    Bottle A(Av,'A'); 
+    Bottle B(Bv,'B');
+   
+    
+    int x;
+    cout << "Введите сколько нужно литров" << endl;
+    cin >> x;
+    if (A.getVolume() > B.getVolume()) {
+        nedNowVolume(x, A, B);
+    }
+    else {
+        nedNowVolume(x, B, A);
+    }
+ 
+    
+    system("pause");
+    return 0;
+}
+
